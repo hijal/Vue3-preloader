9dacfdfbeb35c0d2a1b0ccc93d9c2a2b7c85f0f5
imh1j4l <hijal@shuttlebd.com>
2022-02-14 20:41:21 +0600

    vue 3 preloader

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+	<Preloader />
+</template>
+
+<script>
+import Preloader from './components/Preloader.vue';
+
+export default {
+	name: 'App',
+	components: {
+		Preloader,
+	},
+};
+</script>
+
+<style>
+#app {
+	position: absolute;
+	left: 50%;
+	top: 50%;
+	margin: 0 auto;
+}
+</style>
